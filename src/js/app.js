fdd1edbbfe1c73ab28767b2a842c793e125d594b
Maria Cosmen <mcosme00@gmail.com>
2021-06-20 23:44:16 +0900

    dark mode JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   eventListeners();
+  darkMode();
 });
+
+function darkMode() {
+  const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+  btnDarkMode.addEventListener("click", () => {
+    const button = document.getElementsByClassName("button");
+    document.body.classList.toggle("dark-mode");
+    for (let i = 0; i < button.length; i++) {
+      button[i].classList.toggle("dark-button");
+    }
+  });
+}
 
 function eventListeners() {
   const mobileMenu = document.querySelector(".mobile-menu");
